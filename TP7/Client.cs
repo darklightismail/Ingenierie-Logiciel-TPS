2833f55bed4e133926c8e7881770577c31ed08fe
Ismail Jabri <47457939+darklightismail@users.noreply.github.com>
2019-12-22 00:10:46 +0100

    Add files via upload

---
+++ TP7/Client.cs
@@ -0,0 +1,72 @@
+// FILE: C:/Users/darklight-ismail/Desktop//Client.cs
+
+// In this section you can add your own using directives
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000866 begin
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000866 end
+
+/// <summary>
+    ///  A class that represents ...
+    /// 
+    ///  @see OtherClasses
+    ///  @author your_name_here
+     /// </summary>
+public class Client
+{
+    // Attributes
+
+    public string nom;
+
+    public string prenom;
+
+    public int numero;
+
+    // Operations
+
+    /// <summary>
+    ///  An operation that does...
+    /// 
+    ///  @param firstParam a description of this parameter
+    /// </summary>
+    /// <returns>
+    /// </returns>
+    public  string getname()
+    {
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000872 begin
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000872 end
+
+    }
+
+    /// <summary>
+    ///  An operation that does...
+    /// 
+    ///  @param firstParam a description of this parameter
+    /// </summary>
+    /// <returns>
+    /// </returns>
+    public  string getprenom()
+    {
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000874 begin
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000874 end
+
+    }
+
+    /// <summary>
+    ///  An operation that does...
+    /// 
+    ///  @param firstParam a description of this parameter
+    /// </summary>
+    /// <param name="numero">
+    /// </param>
+    /// <param name="nom">
+    /// </param>
+    /// <param name="prenom">
+    /// </param>
+    /// <returns>
+    /// </returns>
+    public Client( numero,  nom,  prenom)
+    {
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000876 begin
+    // section -64--88-126-1--1d36e86e:16eeaf1c82d:-8000:0000000000000876 end
+
+    }
+} /* end class Client */
